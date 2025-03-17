97d4c063b5b5e43f556b7056051f8b5442ef6904
letuanGithubVn1 <letuansg.07@gmail.com>
2025-03-17 18:27:16 +0700

    auto commit

--- expense-management/src/main/java/dto/request_transaction_DTO.java
+++ expense-management/src/main/java/dto/request_transaction_DTO.java
@@ -8,6 +8,18 @@ import lombok.Data;
 @Data
 public class request_transaction_DTO {
 	
+	private static request_transaction_DTO instance;
+
+    private request_transaction_DTO() {}
+
+    // Phương thức để lấy đối tượng duy nhất 
+    public static request_transaction_DTO getInstance() {
+        if (instance == null) {
+            instance = new request_transaction_DTO(); 
+        }
+        return instance;
+    }
+	
 	@Data
 	public static class transactionRequest{
 		private Long categoryId;
--- expense-management/src/main/java/dto/response_transaction_DTO.java
+++ expense-management/src/main/java/dto/response_transaction_DTO.java
@@ -5,6 +5,19 @@ import lombok.Data;
 
 @Data
 public class response_transaction_DTO {
+	
+	private static response_transaction_DTO instance;
+
+    private response_transaction_DTO() {}
+
+    // Phương thức để lấy đối tượng duy nhất 
+    public static response_transaction_DTO getInstance() {
+        if (instance == null) {
+            instance = new response_transaction_DTO(); 
+        }
+        return instance;
+    }
+    
 	@Data
     @AllArgsConstructor
     public static class TransactionStatusResponse {
